84d5db5812e410bcb33ef373f0a602da3ee0838f
xingk <xingk@getui.com>
2017-08-23 17:04:46 +0800

    向阿里云发送请求时增加时间戳字段（Timestamp)

--- aliyun/api/rest/Dns20150109UpdateDomainRecordRequest.py
+++ aliyun/api/rest/Dns20150109UpdateDomainRecordRequest.py
@@ -2,8 +2,10 @@
 Created by auto_sdk on 2015.09.21
 '''
 from aliyun.api.base import RestApi
+import time
 class Dns20150109UpdateDomainRecordRequest(RestApi):
 	def __init__(self,address,domain='dns.aliyuncs.com',port=80):
+		timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
 		RestApi.__init__(self,domain, port)
 		self.Line = "default"
 		self.Priority = None
@@ -12,6 +14,7 @@ class Dns20150109UpdateDomainRecordRequest(RestApi):
 		self.TTL = None
 		self.Type = "A"
 		self.Value = address
+		self.Timestamp = timestamp
 
 	def getapiname(self):
 		return 'dns.aliyuncs.com.UpdateDomainRecord.2015-01-09'
